1dbc57a896425d6ee0a18819ea2dfbe2d2b55a29
kittipon-dev <kittipon.cc@gmail.com>
2022-11-10 23:46:33 +0700

    Update index.js

--- index.js
+++ index.js
@@ -118,7 +118,7 @@ async function createHistory() {
 
 setInterval(async () => {
   createHistory();
-}, 3000);
+}, 5000);
 
 // app.listen(3000, () => {
 //   console.log("Listening on port 3000");
